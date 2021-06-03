d6412348d56a5f647490f1687cbb8653b41130c3
Justin Bennett <zephraph@gmail.com>
2021-06-02 23:39:05 -0400

    Tweek jest settings to run at the project level

--- packages/create-obsidian-plugin/__tests__/create-obsidian-plugin.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const createObsidianPlugin = require('..');
-
-describe('create-obsidian-plugin', () => {
-    it('needs tests');
-});
--- packages/obsidian-utils/src/__tests__/utils.test.ts
+++ packages/obsidian-utils/src/__tests__/utils.test.ts
@@ -2,7 +2,7 @@ import { windowsToWSLPath } from "../utils";
 
 describe("windowsToWSLPath", () => {
   it("converts a windows path to a WSL path", () => {
-    expect(windowsToWSLPath("C:\\\\Users\\\\User\\\\notes")).toEqual(
+    expect(windowsToWSLPath("C:\\Users\\User\\notes")).toEqual(
       "/mnt/c/Users/User/notes"
     );
   });
